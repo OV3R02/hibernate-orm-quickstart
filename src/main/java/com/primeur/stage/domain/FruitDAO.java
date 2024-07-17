3d2fb68cf457b375ff341fc5e132a333605ca518
francesco.divono <francesco.divono@primeur.com>
2024-07-17 17:26:06 +0200

    test refactor in esagonale

--- src/main/java/com/primeur/stage/domain/FruitDAO.java
+++ src/main/java/com/primeur/stage/domain/FruitDAO.java
@@ -3,9 +3,11 @@ package com.primeur.stage.domain;
 import com.primeur.stage.domain.dto.Fruit;
 import com.primeur.stage.port.FruitStorageService;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 
+@ApplicationScoped
 public class FruitDAO implements FruitStorageService {
 
 	@Inject
